988a2650a13683a3d391c2fbcae8e9619e213a21
qd58 <rajiv.bissonauth@sunlife.com>
2024-06-14 14:28:42 -0400

    training: snowflake_training_step_8b

--- main.tf
+++ main.tf
@@ -25,6 +25,6 @@ resource "snowflake_database" "demo_db" {
 
 resource "snowflake_schema" "demo_schema" {
   database = snowflake_database.demo_db.name
-  name     = "DEMO_DB_R_KOYKKA"
+  name     = "DEMO_SCHEMA_R_KOYKKA"
   comment  = "Schema for Snowflake Terraform demo"
 }
